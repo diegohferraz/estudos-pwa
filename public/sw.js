1d8273a0e46cff10b84619a232574b1b40d0418f
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-11-06 18:37:24 -0300

    adds cache cleanup strategies

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
         console.log('[SW]: precaching app shell')
         cache.addAll([
@@ -24,6 +27,17 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[SW] Removing old cache', key)
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
   return self.clients.claim();
 });
 
@@ -36,11 +50,14 @@ self.addEventListener('fetch', function(event) {
         } else {
           return fetch(event.request)
             .then(function(res) {
-              return caches.open('dynamic')
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache) {
-                  cache.put(event.request.url, res)
+                  cache.put(event.request.url, res.clone())
                   return res
                 })
+            })
+            .catch(function() {
+
             })
         }
       })
