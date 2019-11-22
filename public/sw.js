5666dfd4cb6d1dac6b2f388bcb382a32051e174d
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2019-11-22 16:28:00 -0300

    adds logic to save on indexedDb

--- public/sw.js
+++ public/sw.js
@@ -20,6 +20,12 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', {keyPath: 'id'});
+  }
+});
+
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
 //     .then(function (cache) {
@@ -75,16 +81,23 @@ self.addEventListener('fetch', function (event) {
 
   var url = 'https://estudos-dd8db.firebaseio.com/';
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              // trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request, res.clone());
-              return res;
-            });
-        })
+    event.respondWith(fetch(event.request)
+      .then(function (res) {
+        var clonedRes = res.clone();
+        clonedRes.json()
+          .then(function(data) {
+            for (var key in data) {
+              dbPromise
+                .then(function(db) {
+                  var tx = db.transaction('posts', 'readwrite');
+                  var store = tx.objectStore('posts');
+                  store.put(data[key]);
+                  return tx.complete;
+                })
+            }
+          });
+        return res;
+      })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
     event.respondWith(
